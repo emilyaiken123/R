065d9d368a6d404898b6e2c9d502dcd80e9bce88
Emily <catpetter1992@gmail.com>
2022-10-22 15:48:08 -0400

    Create 3-1-1 Confidence intervals.R

---
+++ Data Science/4 - Inference and Modeling/3-1-1 Confidence intervals.R
@@ -0,0 +1,8 @@
+library(ggplot2)
+
+data("nhtemp")
+data.frame(year = as.numeric(time(nhtemp)), temperature = as.numeric(nhtemp)) %>%
+  ggplot(aes(year, temperature)) +
+  geom_point() +
+  geom_smooth() +
+  ggtitle("Average Yearly Temperatures in New Haven")  
